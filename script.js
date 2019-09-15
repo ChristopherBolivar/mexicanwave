0a8051cafc0cbd6a8e4d1b91b8b20a3b5da72854
chris bolivar <christopherbolivar@hotmail.com>
2019-09-15 13:11:59 -0400

    got some waves going but yet complete

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function wave(a){
-    console.log(a)
-    let count = 0;
-    let narr =[]
+    var narr =[]
+    let rarr =[]
+    var word = ""
+    var count = 0
+    var it = 0
+    function pushThis (str){  
+     str.split("").forEach(c =>{
+        if(str.indexOf(c) != it){
+            word += c
+        }else {
+          word += c.toUpperCase()
+        }
+        if(str.indexOf(str[count]) === (a.length -1) ){
+            count++
+        }
+     })
+
+     it++
+     rarr.push(word)
+     word = ""
+}   
     a.split("").forEach(b => {
-        // console.log(b)
-      if(a.indexOf(b) === count){
-          console.log(true)
-      }
-    })
+        pushThis(a.toString().replace(/,/g,""))
+     })
+    console.log(rarr)
+    
   }
   wave("hello")
