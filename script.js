b583b7a42b8b71ea2661a602a5bff0505813ff98
chris bolivar <christopherbolivar@hotmail.com>
2019-09-16 21:14:06 -0400

    rewrote my script, used different approach when looping

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
 function wave(a){
     var narr =[]
-    let rarr =[]
     var word = ""
-    var count = 0
     var it = 0
-    function pushThis (str){  
-     str.split("").forEach(c =>{
-        if(str.indexOf(c) != it){
-            word += c
-        }else {
-          word += c.toUpperCase()
-        }
-        if(str.indexOf(str[count]) === (a.length -1) ){
-            count++
-        }
-     })
 
-     it++
-     rarr.push(word)
-     word = ""
-}   
-    a.split("").forEach(b => {
-        pushThis(a.toString().replace(/,/g,""))
-     })
-    console.log(rarr)
+//     //looping thru passed argument
+//     a.split("").forEach(b => {
+//     //passing the argument to the function pushThis
+//      pushThis(a.toString().replace(/,/g,""))
+     
+//      })
+     
+//     function pushThis (str){  
+//     //looping thru passed argument
+//      str.split("").forEach(c =>{
+//         // logging index of each character
+//          console.log(str.indexOf(c), c)
+//          //conditionals matching index to counter iterarator
+//         if((str.indexOf(c)) === it){
+//             word += c.toUpperCase()
+//         }else{
+//           word += c.toLowerCase()
+//         }
+      
+//      })
+
+//      it++ //increasing iterator each time function is called
+//      narr.push(word) //pushing word to new array
+//      word = "" //clearing word variable
+// }   
     
-  }
+   
+ a.split("").map((letter,i)=>{
+   let aCopy = a.split("")
+   
+//    console.log(a.split("").indexOf(letter, i) , i)
+   if(i === aCopy.length - 1){
+    it++
+   }
+   if(aCopy.indexOf(letter, i) === it){
+    word += letter.toUpperCase()
+   }else{
+     word += letter.toLowerCase()
+ }
+ 
+   console.log(it , aCopy.indexOf(letter, i))
+ })
+
+narr.push(word)
+console.log(narr)
+}
+
   wave("hello")
